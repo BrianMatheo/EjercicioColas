d0d7e092ba7e0ffacecf1685fff0b23143932ceb
Brian Matheo <bmalvarezp@ufpso.edu.co>
2024-10-22 06:55:49 -0500

    v1

---
+++ src/main/java/com/mycompany/colasejercicio/ColasEjercicio.java
@@ -0,0 +1,13 @@
+package com.mycompany.colasejercicio;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ColasEjercicio {
+
+    public static void main(String[] args) {
+        Queue<String> lista = new LinkedList<>();
+        
+        
+    }
+}
